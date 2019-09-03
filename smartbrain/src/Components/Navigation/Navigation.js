150f673241ca708a59e3d81ab71083d9b26dac61
zlawson13 <zach@executivedsllc.com>
2019-09-03 08:58:08 -0500

    Tachyon

---
+++ smartbrain/src/Components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p>Sign Out</p>
+        </nav>
+    );
+}
+
+
+export default Navigation
