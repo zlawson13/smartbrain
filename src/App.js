15f1d95501d931b1fa7ad9b875fb3c306f533cd4
zlawson13 <zach@executivedsllc.com>
2019-09-03 12:20:54 -0500

    Finished Front-End

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import Particles from 'react-particles-js';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+        <Particles className='particles'
+                params={particleOptions} />
       <Navigation />
-      <Logo /> 
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      */FaceRecogntion />*/
+
+  {/*FaceRecogntion />*/}
     </div>
   );
 }
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Rank';
+
+const Rank = () => {
+  return (
+      <div>
+          <div className='white f3'>
+              {'Zach, Your Current Rank Is'}
+          </div>
+          <div className='white f1'>
+              {'#5'}
+          </div>
+      </div>
+  );
+}
+
+
+export default Rank
