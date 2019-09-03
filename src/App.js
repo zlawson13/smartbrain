43525f80f67046acbc8187f5d9b4a73795d381bf
zlawson13 <zach@executivedsllc.com>
2019-09-03 11:49:21 -0500

    Updated Tachyons 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
-              <ImageLinkForm />
-              <FaceRecogntion />*/}
+      <Logo /> 
+      <ImageLinkForm />
+      */FaceRecogntion />*/
     </div>
   );
 }
---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ImageLinkForm.css'
+
+const ImageLinkForm = () => {
+  return (
+      <div>
+          <p className='f3'>
+              {'This Magic Brain Will Detect Faces In Your Pictures. Give It A Try!'}
+          </p>
+          <div className='center'>
+              <div className="form pa4 br3 shadow-5 center"></div>
+              <input className= 'f4 pa2 w-70 center' type='text' />
+              <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+          </div>
+      </div>
+  );
+}
+
+export default ImageLinkForm;
