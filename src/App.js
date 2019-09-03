b779090753f5c044e63049a00def3bf091fd75b2
zlawson13 <zach@executivedsllc.com>
2019-09-03 11:49:02 -0500

    Update Tachyons

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*<Logo />
+              <ImageLinkForm />
+              <FaceRecogntion />*/}
+    </div>
+  );
+}
+
+export default App;
