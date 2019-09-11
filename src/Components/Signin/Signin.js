2497ae3a842902f81c180ac062953f1a52483653
Nevlaa <shorsburgh19@gmail.com>
2019-09-10 19:31:55 -0600

    update component name

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://agile-plains-23734.herokuapp.com/signin', {
+    fetch('https://agile-plains-23734.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
